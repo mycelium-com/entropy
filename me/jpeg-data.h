9f222d641d8392ac4f37690e9515c4b0d0c235d5
Nikita Schmidt <nikita@megiontechnologies.com>
2015-02-26 18:48:33 +0200

    Added automatically generated files.
    The generator script uses fonts that may not be installed in everybody's
    build environment.

---
+++ me/jpeg-data.h
@@ -0,0 +1,151 @@
+// Automatically generated JPEG data file.
+// Size: 1808x2432 pixels / 226x304 macroblocks.
+// Header font: Optima Regular, 44 pt.
+// Data font:   Menlo Regular, 40 pt.
+
+#include <stdint.h>
+
+// Parameters (used in #ifdef hence not an enum):
+#define JWIDTH      226
+#define JHEIGHT     304
+
+// JPEG bitstream elements:
+#define SME {  3,    0x0 }
+#define WTB { 16, 0xc01c }
+#define BTW { 16, 0xdfe0 }
+#define AC_EOB_LEN       2
+#define AC_EOB_CODE    0x0
+enum {
+    SME_LEN             = 3,
+    SME_BITS            = 0x0,
+    SME_LONG_MB         = 8,
+    SME_LONG_LEN        = 24,
+    SME_LONG_BITS       = 0x0,
+    GTW_LEN             = 15,
+    GTW_BITS            = 0x5fe0,
+};
+
+// Huffman DC table 0
+struct Huffman_table {
+    uint16_t len;
+    uint16_t code;
+};
+extern const struct Huffman_table huff_dc[12];
+
+// Font data encoded as JPEG bitstream
+
+enum {
+    CHR_WIDTH   = 3,
+    CHR_HEIGHT  = 5,
+};
+
+struct Chr_row_descr {
+    int16_t dc_in;
+    int16_t dc_out;
+    uint8_t nbytes;
+    uint8_t nbits;
+    uint16_t offset;
+};
+
+extern const struct Chr_row_descr addr_font[64][CHR_HEIGHT];
+extern const unsigned char font_map[91];
+extern const unsigned char addr_font_data[35984];
+
+// Pictures.
+extern const uint16_t cut_here_fragment[];
+enum {
+    CUT_HERE_FRAGMENT_HEIGHT = 5
+};
+extern const uint16_t bitcoin_fragment[];
+enum {
+    BITCOIN_FRAGMENT_WIDTH  = 18,
+    BITCOIN_FRAGMENT_HEIGHT = 5
+};
+extern const uint16_t litecoin_fragment[];
+enum {
+    LITECOIN_FRAGMENT_WIDTH  = 20,
+    LITECOIN_FRAGMENT_HEIGHT = 5
+};
+extern const uint16_t peercoin_fragment[];
+enum {
+    PEERCOIN_FRAGMENT_WIDTH  = 22,
+    PEERCOIN_FRAGMENT_HEIGHT = 5
+};
+extern const uint16_t address_fragment[];
+enum {
+    ADDRESS_FRAGMENT_WIDTH  = 20,
+    ADDRESS_FRAGMENT_HEIGHT = 5
+};
+extern const uint16_t account_fragment[];
+enum {
+    ACCOUNT_FRAGMENT_WIDTH  = 21,
+    ACCOUNT_FRAGMENT_HEIGHT = 5
+};
+extern const uint16_t private_key_fragment[];
+enum {
+    PRIVATE_KEY_FRAGMENT_WIDTH  = 28,
+    PRIVATE_KEY_FRAGMENT_HEIGHT = 7
+};
+extern const uint16_t share_1_of_3_fragment[];
+enum {
+    SHARE_1_OF_3_FRAGMENT_WIDTH  = 30,
+    SHARE_1_OF_3_FRAGMENT_HEIGHT = 5
+};
+extern const uint16_t share_2_of_3_fragment[];
+enum {
+    SHARE_2_OF_3_FRAGMENT_WIDTH  = 30,
+    SHARE_2_OF_3_FRAGMENT_HEIGHT = 5
+};
+extern const uint16_t share_3_of_3_fragment[];
+enum {
+    SHARE_3_OF_3_FRAGMENT_WIDTH  = 30,
+    SHARE_3_OF_3_FRAGMENT_HEIGHT = 5
+};
+extern const uint16_t any_two_shares_reveal_fragment[];
+enum {
+    ANY_TWO_SHARES_REVEAL_FRAGMENT_WIDTH  = 74,
+    ANY_TWO_SHARES_REVEAL_FRAGMENT_HEIGHT = 7
+};
+extern const uint16_t seed_fragment[];
+enum {
+    SEED_FRAGMENT_WIDTH  = 13,
+    SEED_FRAGMENT_HEIGHT = 5
+};
+extern const uint16_t entropy_for_verification_fragment[];
+enum {
+    ENTROPY_FOR_VERIFICATION_FRAGMENT_WIDTH  = 57,
+    ENTROPY_FOR_VERIFICATION_FRAGMENT_HEIGHT = 7
+};
+extern const uint16_t key_sha_256_salt1_fragment[];
+enum {
+    KEY_SHA_256_SALT1_FRAGMENT_WIDTH  = 77,
+    KEY_SHA_256_SALT1_FRAGMENT_HEIGHT = 7
+};
+extern const uint16_t small_print_fragment[];
+enum {
+    SMALL_PRINT_FRAGMENT_WIDTH  = 84,
+    SMALL_PRINT_FRAGMENT_HEIGHT = 5
+};
+extern const uint16_t keep_safe_fragment[];
+enum {
+    KEEP_SAFE_FRAGMENT_WIDTH  = 21,
+    KEEP_SAFE_FRAGMENT_HEIGHT = 5
+};
+extern const uint16_t logo_top_fragment[];
+enum {
+    LOGO_TOP_FRAGMENT_WIDTH  = 10,
+    LOGO_TOP_FRAGMENT_HEIGHT = 20
+};
+extern const uint16_t logo_middle_fragment[];
+enum {
+    LOGO_MIDDLE_FRAGMENT_WIDTH  = 42,
+    LOGO_MIDDLE_FRAGMENT_HEIGHT = 25
+};
+extern const uint16_t logo_bottom_fragment[];
+enum {
+    LOGO_BOTTOM_FRAGMENT_WIDTH  = 66,
+    LOGO_BOTTOM_FRAGMENT_HEIGHT = 13
+};
+
+// JPEG header.
+extern const unsigned char jpeg_header[423];
